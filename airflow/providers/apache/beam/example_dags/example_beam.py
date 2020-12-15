b65436855b0d07a0c07b88110fdb6a863944e097
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-12-15 10:39:42 +0100

    Bump Beam version to latest 2.26.0 in example dag

--- airflow/providers/apache/beam/example_dags/example_beam.py
+++ airflow/providers/apache/beam/example_dags/example_beam.py
@@ -200,7 +200,7 @@ with models.DAG(
         task_id="start_python_job_local_direct_runner",
         py_file='apache_beam.examples.wordcount',
         py_options=['-m'],
-        py_requirements=['apache-beam[gcp]==2.25.0'],
+        py_requirements=['apache-beam[gcp]==2.26.0'],
         py_interpreter='python3',
         py_system_site_packages=False,
     )
@@ -210,7 +210,7 @@ with models.DAG(
         py_file=GCS_PYTHON,
         py_options=[],
         pipeline_options={"output": GCS_OUTPUT},
-        py_requirements=['apache-beam[gcp]==2.25.0'],
+        py_requirements=['apache-beam[gcp]==2.26.0'],
         py_interpreter='python3',
         py_system_site_packages=False,
     )
@@ -226,7 +226,7 @@ with models.DAG(
         },
         py_options=[],
         job_name='{{task.task_id}}',
-        py_requirements=['apache-beam[gcp]==2.25.0'],
+        py_requirements=['apache-beam[gcp]==2.26.0'],
         py_interpreter='python3',
         py_system_site_packages=False,
     )
@@ -236,7 +236,7 @@ with models.DAG(
         py_file='apache_beam.examples.wordcount',
         runner="SparkRunner",
         py_options=['-m'],
-        py_requirements=['apache-beam[gcp]==2.25.0'],
+        py_requirements=['apache-beam[gcp]==2.26.0'],
         py_interpreter='python3',
         py_system_site_packages=False,
     )
@@ -249,7 +249,7 @@ with models.DAG(
         pipeline_options={
             'output': '/tmp/start_python_job_local_flink_runner',
         },
-        py_requirements=['apache-beam[gcp]==2.25.0'],
+        py_requirements=['apache-beam[gcp]==2.26.0'],
         py_interpreter='python3',
         py_system_site_packages=False,
     )
